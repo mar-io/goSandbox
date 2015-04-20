d43beb869bdcf0afe32218721ac956fa75a814d2
Mario Harvey <mario.j.harvey@accenture.com>
2015-04-20 15:56:28 -0500

    adding variadic function

---
+++ src/martini/fizzbuzz.go
@@ -0,0 +1,18 @@
+package main 
+
+import "fmt"
+
+func main() {
+  for i := 0; i <= 100; i++ {
+    if (i%15 == 0) {
+      fmt.Println(i, "is fizzbuzz")
+    } else if (i%5 == 0) {
+      fmt.Println(i, "is fizz")
+    } else if (i%3 == 0) {
+      fmt.Println(i, "is buzz")
+    } else {
+      fmt.Println(i, "is no exists")
+    }
+  }
+  
+}
